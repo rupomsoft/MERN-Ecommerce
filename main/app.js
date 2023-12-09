487a7db1c1f456b988c7b483026aab2068a54d12
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-12-09 17:25:46 +0600

    sss
    sss

---
+++ main/app.js
@@ -0,0 +1,64 @@
+const express =require('express');
+const router =require('./src/routes/api');
+const app= new express();
+
+const rateLimit =require('express-rate-limit');
+const helmet =require('helmet');
+const mongoSanitize =require('express-mongo-sanitize');
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose =require('mongoose');
+const path = require("path");
+
+<<<<<<< Updated upstream
+=======
+
+//let URL="mongodb://127.0.0.1:27017/ecom4?retryWrites=true&w=majority"
+/*
+mongoose.connect(URL).then((res)=>{
+    console.log("Database Connected")
+}).catch((err)=>{
+    console.log(err)
+})
+ */
+
+>>>>>>> Stashed changes
+let URL="mongodb+srv://<username>:<password>@cluster0.7uslu.mongodb.net/ecom4?retryWrites=true&w=majority";
+let option={user:'testuser7777',pass:"testuser7777",autoIndex:true};
+mongoose.connect(URL,option).then((res)=>{
+    console.log("Database Connected")
+}).catch((err)=>{
+    console.log(err)
+})
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
+
+
+app.use(cookieParser());
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
+const limiter= rateLimit({windowMs:15*60*1000,max:3000})
+app.use(limiter)
+
+
+app.use("/api/v1",router)
+
+
+// Add React Front End Routing
+app.get('*',function (req,res) {
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
+
+module.exports=app;
