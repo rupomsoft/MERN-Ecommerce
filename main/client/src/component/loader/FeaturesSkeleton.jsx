478ae4c9a0b1cd03389381b31cb63882dfe6eff2
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2024-01-08 17:35:22 +0600

    asd
    asdad

--- main/client/src/component/loader/FeaturesSkeleton.jsx
+++ main/client/src/component/loader/FeaturesSkeleton.jsx
@@ -50,6 +50,21 @@ const FeaturesSkeleton = () => {
                         </div>
                     </div>
                 </div>
+				
+				                <div className="col-6 p-2 col-md-3 col-lg-3 col-sm-6">
+                    <div className="card shadow-sm">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-4">
+                                    <Lottie  className="w-100" animationData={ImagePlaceholder} loop={true} />
+                                </div>
+                                <div className="col-8">
+                                    <Skeleton count={3} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
